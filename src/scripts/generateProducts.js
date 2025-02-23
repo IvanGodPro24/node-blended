755286489a62baa54ee5445007bcb4732428567a
IvanGodPro24 <vanyanep@gmail.com>
2025-02-23 10:47:43 +0200

    Add first part

---
+++ src/scripts/generateProducts.js
@@ -0,0 +1,23 @@
+import { DB_PATH } from "../constants/index.js";
+import fs from "node:fs/promises";
+import { createFakeProduct } from "../utils/createFakeProduct.js";
+
+export const generateProducts = async (number) => {
+  try {
+    const productsData = await fs.readFile(DB_PATH, "utf-8");
+
+    const products = JSON.parse(productsData);
+
+    for (let i = 0; i < number; i += 1) {
+      const product = createFakeProduct();
+
+      products.push(product);
+
+      await fs.writeFile(DB_PATH, JSON.stringify(products, undefined, 2));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+generateProducts(5);
